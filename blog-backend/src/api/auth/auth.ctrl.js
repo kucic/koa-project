9bd0e442d6134fce004c65a6aaec82da41df5b20
ku minchul <punkblood9@gmail.com>
2022-09-06 15:35:33 +0900

    auth 구현, jwt 생성 함수 추가

--- blog-backend/src/api/auth/auth.ctrl.js
+++ blog-backend/src/api/auth/auth.ctrl.js
@@ -1,9 +1,88 @@
+import Joi from 'joi';
+import User from '../../models/user.js';
+
+/**
+  POST /api/auth/register
+  {
+    username: 'velopert',
+    password: 'mypass123'
+  }
+ */
 export const register = async ctx => {
   // 회원가입
-}
+  // Request Body 검증
+  const schema = Joi.object().keys({
+    username: Joi.string()
+      .alphanum()
+      .min(3)
+      .max(20)
+      .required(),
+    password: Joi.string().required(),
+  });
+  const result = schema.validate(ctx.request.body);
+  if(result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
 
+  const {username, password} = ctx.request.body;
+  try{
+    // username 이 이미 존재하는지 확인
+    const exists = await User.findByUsername(username);
+    if(exists) {
+      ctx.status = 409; // conflict
+      return;
+    }
+
+    const user = new User({
+      username,
+    })
+    await user.setPassword(password);
+    await user.save();
+
+    //응답할 데이터에서 hashedPassword 필드 제거
+    // const data = user.toJSON();
+    // delete data.hashedPassword;
+    // ctx.body = data;
+    ctx.body = user.serialize();
+  } catch(e) {
+    ctx.throw(500, e);
+  }
+}
+/**
+  POST /api/auth/login
+  {
+    username: 'velopert',
+    password: 'mypass123'
+  }
+ */
 export const login = async ctx => {
   // 로그인
+  const {username, password } = ctx.request.body;
+
+  // username, password 가 없으면 에러처리
+  if(!username || !password) {
+    ctx.status = 401; // Unauthorized
+    return;
+  }
+
+  try{
+    const user = await User.findByUsername(username);
+    // 계정이 존재하지 않으면 에러
+    if(!user) {
+      ctx.status = 401;
+      return;
+    }
+    const valid = await user.checkPassword(password);
+    if(!valid) {
+      ctx.status = 401;
+      return;
+    }
+    ctx.body = user.serialize();
+  }catch(e){
+    ctx.throw(500, e);
+  }
 }
 
 export const check = async ctx => {
--- blog-backend/src/models/user.js
+++ blog-backend/src/models/user.js
@@ -37,6 +37,29 @@ UserSchema.statics.findByUsername = function(username) {
   return this.findOne({username});
 }
 
+// serialize
+UserSchema.methods.serialize = function() {
+  const data = this.toJSON();
+  delete data.hashedPassword;
+  return data;
+}
+
+// token  gelerate
+UserSchema.methods.generateToken = function() {
+  const token = jwt.sign(
+    // 첫 번째 파라미터에는 토큰 안에 집어넣고 싶은 데이터를 넣는다. 
+    {
+      _id: this.id,
+      username: this.username,
+    },
+    process.env.JWT_SECRET, // 두번째 파라미터는 JWT 암호
+    {
+      expiresIn: '7d', // 7일간 유효
+    }
+  )
+  return token;
+}
+
 const User = mongoose.model('User', UserSchema);
 
 export default User;
